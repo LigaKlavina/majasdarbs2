87fda7e89bd6ebf7303f62a25fa79d4e57340952
LigaKlavina <ligaiklavinai@gmail.com>
2018-05-13 12:19:18 +0300

    Initial commit

---
+++ majasdarbs2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace majasdarbs2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n;
+            Console.WriteLine("ierakstit skaitli:");
+            n = int.Parse(Console.ReadLine());
+            for (i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(n + "*" + i + "=" + n * i);
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
